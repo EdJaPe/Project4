cdfd0eec0bfb049971834e1fad5cafe6ea399707
edJaPe <edv06jara@gmail.com>
2021-03-17 16:15:11 -0700

    data rendering and using two different api calls

--- pages/search.js
+++ pages/search.js
@@ -4,13 +4,13 @@ import Toolbar from '../component/toolbar'
 import styles from '../styles/Search.module.css'
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY
-const API_URL_TWO = process.env.NEXT_PUBLIC_API_URL_TWO
+const API_TWO = process.env.NEXT_PUBLIC_API_TWO
 
 export default function Search() {
     const [searchQuery, setSearchQuery] = useState('')
     const [country, setCountry] = useState('')
     const [results, setResults] = useState([])
-    const [searchType, setSearchType ] = useState("city")
+    const [searchType, setSearchType] = useState("city")
 
     var options = {
         method: 'GET',
@@ -18,12 +18,14 @@ export default function Search() {
         params: { postalCode: searchQuery, countryCode: country },
         headers: { 'x-api-key': API_KEY, 'Content-type': 'application/json' }
     };
+
+    // If url changed to API
     var optionTwo = {
         method: 'GET',
         url: 'https://api.ambeedata.com/latest/by-city',
-        params: {city: searchQuery},
-        headers: {'x-api-key': API_KEY, 'Content-type': 'application/json'}
-      };
+        params: { city: searchQuery },
+        headers: { 'x-api-key': API_KEY, 'Content-type': 'application/json' }
+    };
 
 
 
@@ -34,7 +36,7 @@ export default function Search() {
             if (response.data) {
                 console.log(response.data);
                 setResults(response.data.stations)
-                console.log('👏🏼',results)
+                console.log('👏🏼', results)
             }
         }).catch(function (error) {
             console.error(error);
@@ -58,44 +60,52 @@ export default function Search() {
     // )
 
 
-    let respond = results.length > 0 ? results.map((place, i) => { <h3 key={i}>PM25:{place.PM25}</h3> }) : <h3>Your results will be here </h3>
+    let respond = results.length > 0 ? results.map((place, i) => {
+        return (
+            <div key={i}>
+                <p>Particulate Matter 25 million<span>{place.PM25}</span></p>
+                <p>Sulfuric DiOxide <span>{place.SO2}</span></p>
+
+            </div>
+        )
+    }) : <h3>Your results will be here </h3>
 
     const selectedForm = searchType === "zip" ? (<form >
-    <div>
-        <label htmlFor="zipInput" >Zip code</label>
-        <input id="zipCode" type="number" name="zipInput" onChange={event => setSearchQuery(event.target.value)} required />
-    </div>
-    <div>
-        <label htmlFor="country">Country</label>
-        <select onChange={e => setCountry(e.target.value)} name="country">
-            <option value="null"></option>
-            <option value="US">United States</option>
-            <option value="IN">India</option>
-        </select>
-    </div>
-    <button onClick={(e) => apiCall(e)}>Search</button>
-</form>) : (<div>
-                <form>
-                    <label htmlFor="city">City</label>
-                    <input type="text" name="city" onChange={e => setSearchQuery(e.target.value)} required/>
-                    <button onClick={(e) => apiCallTwo(e)}>Search</button>
-                </form>
-            </div>)
+        <div>
+            <label htmlFor="zipInput" >Zip code</label>
+            <input id="zipCode" type="number" name="zipInput" onChange={event => setSearchQuery(event.target.value)} required />
+        </div>
+        <div>
+            <label htmlFor="country">Country</label>
+            <select onChange={e => setCountry(e.target.value)} name="country">
+                <option value="null"></option>
+                <option value="US">United States</option>
+                <option value="IN">India</option>
+            </select>
+        </div>
+        <button onClick={(e) => apiCall(e)}>Search</button>
+    </form>) : (<div>
+        <form>
+            <label htmlFor="city">City</label>
+            <input type="text" name="city" onChange={e => setSearchQuery(e.target.value)} required />
+            <button onClick={(e) => apiCallTwo(e)}>Search</button>
+        </form>
+    </div>)
 
     return (
 
         <div>
             <Toolbar />
             <h1>Check your Area</h1>
-            
-                <div className="searchSelect" onChange={e => setSearchType(e.target.value)}>
-                    <input type="radio" name="choose" value="zip" />ZipCode Search
+
+            <div className="searchSelect" onChange={e => setSearchType(e.target.value)}>
+                <input type="radio" name="choose" value="zip" />ZipCode Search
                     <input type="radio" name="choose" value="city" />City search
                 </div>
-                {selectedForm}
-                <div>
-                    {respond}
-                </div>
+            {selectedForm}
+            <div>
+                {respond}
+            </div>
 
         </div>
     )
